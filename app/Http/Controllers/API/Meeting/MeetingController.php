59a5566dc6179b18615702232203fe54f4269760
emadkingslayer <emad@fandaqah.com>
2024-07-21 20:59:38 +0300

    init meetings

---
+++ app/Http/Controllers/API/Meeting/MeetingController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Meeting;
+
+use App\Models\Meeting;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController;
+
+class MeetingController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Meeting $meeting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Meeting $meeting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Meeting $meeting)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_07_21_162010_create_meetings_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->unsignedBigInteger('user_id')->index()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('schedule_slot_id')->index()->nullable();
+            $table->foreign('schedule_slot_id')->references('id')->on('schedule_slots')->onDelete('cascade');
+            $table->string('meeting_provider')->index()->nullable();
+            $table->text('meeting_url')->nullable();
+            $table->enum('meeting_status', [
+                'pending',
+                'awaiting-payment',
+                'confirmed',
+                'started',
+                'in-progress',
+                'canceled',
+                'timeout',
+                'finished'
+            ])->default('pending')->index();
+            // later i need to add ref here for the transaction     
+            $table->date('meeting_date')->index()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meetings');
+    }
+};
