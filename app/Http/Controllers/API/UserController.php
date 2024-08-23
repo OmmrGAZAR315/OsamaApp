2fe5d41c1977fa89ed55679a10263ec3b6100cd7
= <=>
2024-08-24 01:32:55 +0300

    revert

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -42,34 +42,14 @@ class UserController extends BaseController
 
     public function generateRtcToken(RtcTokenRequest $request)
     {
-        $appId = $request->get('app_id');
+        $appID = $request->get('app_id');
         $appCertificate = $request->get('app_certificate');
         $channelName = $request->get('channel_name');
         $expiresInSeconds = $request->get('token_expire_in_seconds') ?? 60;
         $uid = (string) Str::uuid();
         $role = RtcTokenBuilder2::ROLE_PUBLISHER;
 
-        $channelName = "7d72365eb983485397e3e3f9d460bdda";
-        $uid = 2882341273;
-        $uidStr = "2882341273";
-        $tokenExpirationInSeconds = 3600;
-        $privilegeExpirationInSeconds = 3600;
-        $joinChannelPrivilegeExpireInSeconds = 3600;
-        $pubAudioPrivilegeExpireInSeconds = 3600;
-        $pubVideoPrivilegeExpireInSeconds = 3600;
-        $pubDataStreamPrivilegeExpireInSeconds = 3600;
-
-        $token1 = RtcTokenBuilder2::buildTokenWithUid($appId, $appCertificate, $channelName, $uid, RtcTokenBuilder2::ROLE_PUBLISHER, $tokenExpirationInSeconds, $privilegeExpirationInSeconds);
-
-        $token2 = RtcTokenBuilder2::buildTokenWithUserAccount($appId, $appCertificate, $channelName, $uidStr, RtcTokenBuilder2::ROLE_PUBLISHER, $tokenExpirationInSeconds, $privilegeExpirationInSeconds);
-
-        $token3 = RtcTokenBuilder2::buildTokenWithUidAndPrivilege($appId, $appCertificate, $channelName, $uid, $tokenExpirationInSeconds, $joinChannelPrivilegeExpireInSeconds, $pubAudioPrivilegeExpireInSeconds, $pubVideoPrivilegeExpireInSeconds, $pubDataStreamPrivilegeExpireInSeconds);
-
-        $token4 = RtcTokenBuilder2::buildTokenWithUserAccountAndPrivilege($appId, $appCertificate, $channelName, $uidStr, $tokenExpirationInSeconds, $joinChannelPrivilegeExpireInSeconds, $pubAudioPrivilegeExpireInSeconds, $pubVideoPrivilegeExpireInSeconds, $pubDataStreamPrivilegeExpireInSeconds);
-
-
-
-        $token = RtcTokenBuilder2::buildTokenWithUid($appId, $appCertificate, $channelName, $uid, $role, $expiresInSeconds);
+        $token = RtcTokenBuilder2::buildTokenWithUid($appID, $appCertificate, $channelName, $uid, $role, $expiresInSeconds);
 
         if($token) {
 
@@ -78,18 +58,7 @@ class UserController extends BaseController
                 $meeting->rtc_token = $token;
                 $meeting->save();
             }
-            return $this->sendResponse(
-                [
-                    'token' => $token,
-                    'other_tokens' => [
-                       'buildTokenWithUid' =>  $token1,
-                       'buildTokenWithUserAccount' => $token2,
-                       'buildTokenWithUidAndPrivilege' => $token3,
-                       'buildTokenWithUserAccountAndPrivilege' => $token4
-                    ]
-                ],
-                'Rtc Token Generated'
-            );
+            return $this->sendResponse($token, 'Rtc Token Generated');
         }
 
         return $this->sendError('We can not generate a token', []);
