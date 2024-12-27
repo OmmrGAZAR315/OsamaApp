cf3fc022c3e9ed16035687d99beb6cfdb40da52a
= <=>
2024-12-27 20:29:42 +0200

    add subscription

--- app/Http/Controllers/API/Payment/PaymobController.php
+++ app/Http/Controllers/API/Payment/PaymobController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\API\Payment;
 use App\Http\Controllers\API\BaseController;
 use App\Models\Meeting;
 use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PaymobController extends BaseController
 {
@@ -19,32 +21,44 @@ class PaymobController extends BaseController
 
     public function handleTransaction(Request $request)
     {
+        $validator = validator::make($request->all(), [
+            'user_id' => 'required',
+            'isTicket' => 'required',
+        ]);
+        if ($validator->fails())
+            return $this->sendError('Validation Error.', $validator->errors(), 422);
+
+        if (!$request->get('isTicket')) {
+            User::where('user_id')->update(['subscription' => 1]);
+            return $this->sendResponse([], 'Subscription Updated Successfully');
+        }
+
         $payment_id = $request->get('id'); // should evaluate to APPROVED
         $txn_response_code = $request->get('txn_response_code'); // should evaluate to APPROVED
         $data_message = $request->get('data.message'); // should evaluate to Approved
         $meeting = Meeting::find($request->get('meeting_id'));
-        if(!$meeting){
+        if (!$meeting) {
             return $this->sendError('Meeting is not found', []);
         }
 
         try {
 
-            if($txn_response_code == 'APPROVED' && $data_message == 'Approved'){
-                // store transaction 
+            if ($txn_response_code == 'APPROVED' && $data_message == 'Approved') {
+                // store transaction
                 $transaction = new Transaction();
                 $transaction->user_id = $meeting->user_id;
                 $transaction->meeting_id = $meeting->id;
                 $transaction->payment_id = $payment_id;
-                $transaction->amount = round($request->get('amount_cents') / 100 , 2);
+                $transaction->amount = round($request->get('amount_cents') / 100, 2);
                 $transaction->response = $request->all();
-                $transaction->save(); 
+                $transaction->save();
 
-                // update meeting status 
+                // update meeting status
                 $meeting->meeting_status = 'confirmed';
                 $meeting->save();
 
                 $result = [
-                    'meeting' => $meeting, 
+                    'meeting' => $meeting,
                     'transaction' => [
                         'payment_id' => $transaction->payment_id,
                     ]
@@ -52,10 +66,10 @@ class PaymobController extends BaseController
                 return $this->sendResponse($result, 'Transaction Stored Successfully');
 
             }
-            return $this->sendError('There is an issue with your payment' , []);
+            return $this->sendError('There is an issue with your payment', []);
         } catch (\Throwable $th) {
-           return $this->sendError($th->getMessage() , [] , 422);
+            return $this->sendError($th->getMessage(), [], 422);
         }
-       
+
     }
 }
